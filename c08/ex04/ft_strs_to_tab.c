9424d600d9769774c3ae560ead1aebc05878a59c
jthecontrarian <jthecontrarian@proton.me>
2026-08-18 22:54:04 +0800

    normed c08

--- c08/ex04/ft_strs_to_tab.c
+++ c08/ex04/ft_strs_to_tab.c
@@ -6,28 +6,28 @@
 /*   By: jelau <jelau@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 18:15:39 by jelau             #+#    #+#             */
-/*   Updated: 2026/08/18 20:54:14 by jelau            ###   ########.fr       */
+/*   Updated: 2026/08/18 22:51:12 by jelau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "ft_stock_str.h"
 
-char *ft_strdup(char *src)
+char	*ft_strdup(char *src)
 {
-	int i;
-	char *str;
+	int		i;
+	char	*str;
 
 	i = 0;
-	while(src[i] != '\0')
+	while (src[i] != '\0')
 		i++;
 	str = malloc(sizeof(char) * (i + 1));
 	if (!str)
 		return (NULL);
 	i = 0;
-	while(src[i] != '\0')
+	while (src[i] != '\0')
 	{
-		str[i]=src[i];
+		str[i] = src[i];
 		i++;
 	}
 	str[i] = '\0';
@@ -46,17 +46,17 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-struct s_stock_str *ft_strs_to_tab(int ac, char **av)
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 {
 	t_stock_str	*p;
 	int			i;
-	
+
 	p = malloc(sizeof(t_stock_str) * (ac + 1));
 	if (!p)
 		return (NULL);
 	i = 0;
 	while (i < ac)
-	{	
+	{
 		p[i].size = ft_strlen(av[i]);
 		p[i].str = av[i];
 		p[i].copy = ft_strdup(av[i]);
@@ -67,5 +67,3 @@ struct s_stock_str *ft_strs_to_tab(int ac, char **av)
 	p[i].copy = NULL;
 	return (p);
 }
-
-
--- c08/ex05/ft_show_tab.c
+++ c08/ex05/ft_show_tab.c
@@ -6,7 +6,7 @@
 /*   By: jelau <jelau@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 20:40:17 by jelau             #+#    #+#             */
-/*   Updated: 2026/08/18 20:49:19 by jelau            ###   ########.fr       */
+/*   Updated: 2026/08/18 22:53:23 by jelau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,33 +26,33 @@ void	ft_putstr(char *str)
 	write(1, str, i);
 }
 
-void    ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-    char    str[20];
-    int     i;
+	char	str[20];
+	int		i;
 
-    i = 0;
-    if (nb == -2147483648)
-        return (write(1, "-2147483648", 11), (void)0);
-    if (nb == 0)
-        return (write(1, "0", 1), (void)0);
-    if (nb < 0)
-    {
-        nb = -nb;
-        write(1, "-", 1);
-    }
-    while (nb > 0)
-    {
-        str[i] = (nb % 10) + '0';
-        nb = nb / 10;
-        if (nb != 0)
-            i++;
-    }
-    while (i >= 0)
-        write(1, &str[i--], 1);
+	i = 0;
+	if (nb == -2147483648)
+		return (write(1, "-2147483648", 11), (void)0);
+	if (nb == 0)
+		return (write(1, "0", 1), (void)0);
+	if (nb < 0)
+	{
+		nb = -nb;
+		write(1, "-", 1);
+	}
+	while (nb > 0)
+	{
+		str[i] = (nb % 10) + '0';
+		nb = nb / 10;
+		if (nb != 0)
+			i++;
+	}
+	while (i >= 0)
+		write(1, &str[i--], 1);
 }
 
-void ft_show_tab(struct s_stock_str *par)
+void	ft_show_tab(struct s_stock_str *par)
 {
 	while (par->str != NULL)
 	{
